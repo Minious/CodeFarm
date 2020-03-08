b90cb3d8ab2247bed6f74b06f8fed83a333e81db
Eliot Godard <eliot.godard@gmail.com>
2020-03-08 19:45:51 -0400

    enable inventory to keep track of item count

--- inventoryButton.js
+++ inventoryButton.js
@@ -1,28 +1,46 @@
 class InventoryButton extends Phaser.GameObjects.Container {
-    constructor (scene, x, y, displayWidth, displayHeight, marginIcon, item) {
+    constructor (scene, x, y, displayWidth, displayHeight, marginIcon, itemInventoryIndex) {
         super(scene, x, y);
 
-        let backgroundImage = scene.add.image(0, 0, 'ui_button');
-        backgroundImage.setInteractive();
-        this.backgroundImage = backgroundImage;
-        this.add(backgroundImage);
+        this.backgroundImage = this.scene.add.image(0, 0, 'ui_button');
+        this.backgroundImage.setInteractive();
+        this.add(this.backgroundImage);
 
-        if(item != undefined){
-            this.callback = () => this.scene.game.scene.getScene('ControllerScene').data.set('selectedItem', item);
-            backgroundImage.on('pointerdown', () => {
+        this.itemInventoryIndex = itemInventoryIndex;
+        let inventory = this.scene.game.scene.getScene('ControllerScene').data.get('inventory');
+        let inventoryItemData = inventory[itemInventoryIndex % inventory.length];
+        this.item = inventoryItemData.name;
+        this.quantity = inventoryItemData.quantity;
+
+        if(this.item != undefined){
+            this.callback = () => {
+                this.scene.game.scene.getScene('ControllerScene').data.set('selectedItemInventoryIndex', this.itemInventoryIndex)
+            };
+            this.backgroundImage.on('pointerdown', () => {
                 this.callback();
             });
 
-            let itemData = this.scene.game.scene.getScene('ControllerScene').LIST_ITEM[item];
+            let itemTypeData = this.scene.game.scene.getScene('ControllerScene').LIST_ITEM[this.item];
 
-            let backgroundImageBounds = backgroundImage.getBounds();
-            let itemIcon = scene.add.sprite(0, 0, itemData.texture, itemData.frame);
-            itemIcon.setDisplaySize(
+            let backgroundImageBounds = this.backgroundImage.getBounds();
+            this.itemIcon = this.scene.add.sprite(0, 0, itemTypeData.texture, itemTypeData.frame);
+            this.itemIcon.setDisplaySize(
                 backgroundImageBounds.width - marginIcon,
                 backgroundImageBounds.height - marginIcon
             );
-            this.add(itemIcon);
-            this.itemIcon = itemIcon;
+            this.add(this.itemIcon);
+
+            this.itemCountText = this.scene.add.text(0, backgroundImageBounds.height / 2, this.quantity, {
+                fontSize: '16px',
+                fontFamily: '"Roboto Condensed"',
+                backgroundColor: 'rgba(0, 0, 0, 0.35)',
+                fixedWidth: backgroundImageBounds.width,
+                fixedHeight: 20,
+                align: 'center',
+                resolution: 3
+            });
+            this.itemCountText.setOrigin(0.5, 1);
+            this.add(this.itemCountText);
         }
 
         const { width, height } = this.getBounds();
