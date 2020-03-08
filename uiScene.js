0f54bfa82590af150d58ff22ca2ddfac12984b0a
Eliot Godard <eliot.godard@gmail.com>
2020-03-08 01:02:01 -0500

    refactor inventory buttons creation template + display grid inventory view

--- uiScene.js
+++ uiScene.js
@@ -2,13 +2,48 @@ class UiScene extends Phaser.Scene {
     constructor() {
         super({
             key: "UiScene"
-        })
+        });
     }
 
     preload() {}
 
-    makeInventoryButtons(nbButtons, marginButtons = 8) {
-        let sizeButton = (this.cameras.main.displayWidth - marginButtons) / nbButtons - marginButtons;
+    buildInventoryBar(){
+        let marginButtons = 8;
+        let nbColumns = 10;
+        let nbRows = 1;
+        let sizeButton = (this.cameras.main.displayWidth - marginButtons) / nbColumns - marginButtons;
+        let inventoryBarButtons = this.makeInventoryButtonsGrid(
+            nbColumns,
+            nbRows,
+            marginButtons,
+            this.cameras.main.displayHeight - sizeButton - marginButtons,
+            sizeButton,
+            marginButtons
+        );
+        inventoryBarButtons.forEach(inventoryButton => this.inventoryBarButtons.add(inventoryButton));
+    }
+
+    buildInventoryGrid(){
+        let sizeButton = 50;
+        let marginButtons = 8;
+        let marginButtonsInventoryBar = 8;
+        let nbColumns = 10;
+        let nbRows = 6;
+        let widthGrid = sizeButton * nbColumns + marginButtons * (nbColumns - 1);
+        let heightGrid = sizeButton * nbRows + marginButtons * (nbRows - 1);
+        let sizeButtonInventoryBar = (this.cameras.main.displayWidth - marginButtons) / nbColumns - marginButtons;
+        let inventoryGridButtons = this.makeInventoryButtonsGrid(
+            nbColumns,
+            nbRows,
+            (this.cameras.main.displayWidth - widthGrid) / 2,
+            (this.cameras.main.displayHeight - sizeButtonInventoryBar - marginButtonsInventoryBar - heightGrid) / 2,
+            sizeButton,
+            marginButtons
+        );
+        inventoryGridButtons.forEach(inventoryButton => this.inventoryBarButtons.add(inventoryButton));
+    }
+
+    makeInventoryButtonsGrid(nbColumns, nbRows, x, y, sizeButton, marginButtons) {
         console.log(sizeButton);
         let cropsTypes = [
             {
@@ -52,24 +87,33 @@ class UiScene extends Phaser.Scene {
                 frame: 107
             },
         ];
-        for(let i=0;i<nbButtons;i+=1) {
-            let cropType = cropsTypes[i % cropsTypes.length];
-            let cropInventoryButton = new InventoryButton(
-                this,
-                marginButtons * (i + 1) + sizeButton * (i + 0.5),
-                this.cameras.main.height - sizeButton / 2 - marginButtons,
-                sizeButton,
-                sizeButton,
-                cropType.item,
-                'crops',
-                cropType.frame
-            );
-            this.add.existing(cropInventoryButton);
+        let inventoryGridButtons = [];
+        for(let j=0;j<nbRows;j+=1) {
+            for(let i=0;i<nbColumns;i+=1) {
+                let cropType = cropsTypes[(i + j * nbColumns) % cropsTypes.length];
+                let cropInventoryButton = new InventoryButton(
+                    this,
+                    x + marginButtons * i + sizeButton * (i + 0.5),
+                    y + marginButtons * j + sizeButton * (j + 0.5),
+                    sizeButton,
+                    sizeButton,
+                    cropType.item,
+                    'crops',
+                    cropType.frame
+                );
+                this.add.existing(cropInventoryButton);
+                inventoryGridButtons.push(cropInventoryButton);
+            }
         }
+        return inventoryGridButtons;
     }
 
     create() {
-        this.makeInventoryButtons(10)
+        this.inventoryBarButtons = this.add.group();
+        this.buildInventoryBar();
+
+        this.inventoryGridButtons = this.add.group();
+        this.buildInventoryGrid();
     }
 
     update(time, delta) {
