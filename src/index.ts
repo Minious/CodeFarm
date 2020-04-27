83edd6bd368fe9045cc93e893f682948b88033be
Godard <eng6@PN101915.campus.nist.gov>
2020-04-27 01:34:10 -0400

    add NODE_ENV

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import { run } from "./app/game";
 
+const modeEmoji: string = process.env.NODE_ENV === "production" ? "⚙" : "🚧";
+
+console.log(
+  `${modeEmoji} CodeFarm running in ${process.env.NODE_ENV} mode. ${modeEmoji}`
+);
+
 // Creates the Phaser canvas and launch the game
 run();
